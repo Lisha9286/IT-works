677d724a119433de60d746f5c0713d2bd83cf576
007Alena <alena7846@mail.ru>
2022-07-20 23:22:42 +0300

    js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
+const btn1=document.querySelector ('.filter-vacancy__filter-title')
+const btn2=document.querySelector ('.filter-vacancy__filter-sectitle')
 
-    fetch('data.json')
-        .then(function(res){
-            return res.json()
-        })
-        .catch(error => console.log(error));
+
+btn1.onclick = function()  {
+    document.querySelector(".filter-vacancy__typies").setAttribute("style", "display: block; opacity:1; transition: 1s; height: 100%;");
+    document.querySelector(".filter-vacancy__filter-title").setAttribute("style", "display: none");
+    document.querySelector(".filter-vacancy__filter-sectitle").setAttribute("style", "display: block");
+}
+
+btn2.onclick = function()  {
+    document.querySelector(".filter-vacancy__typies").setAttribute("style", "display: none");
+    document.querySelector(".filter-vacancy__filter-sectitle").setAttribute("style", "display: none");
+    document.querySelector(".filter-vacancy__filter-title").setAttribute("style", "display: block"); 
+}
+
