93f0079dce08d23cb1bce01682c06cb8133e5dea
007Alena <alena7846@mail.ru>
2022-07-21 20:34:25 +0300

    js

--- js/index.js
+++ js/index.js
@@ -15,8 +15,31 @@ btn2.onclick = function()  {
     document.querySelector(".filter-vacancy__filter-title").setAttribute("style", "display: block"); 
 }
 
-fetch("data.json")
-    .then(function (res) {
-        return res.json();
-    })
-    .catch((error) => console.log(error));
+const container = document.querySelector('.search-container');
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getSuperheroes();
+});
+
+function getSuperheroes() {
+    fetch('./data.json')
+        .then(response => response.json())
+        .then(heroes => {
+            let heroesCards = '';
+            for (let hero of heroes) {
+                heroesCards += `
+                <div class="card">
+                    <img class="card__img" src="${hero.albumId}" alt="image">
+                    <div class="card__content">
+                        <p class="card__text">albumId: ${hero.albumId}</p>
+                        <p class="card__text">title: ${hero.albumId}</p>
+                    </div>
+                </div>
+                    `
+
+            }
+            container.innerHTML = heroesCards;
+
+        })
+        .catch(error => console.log(error));
+}
