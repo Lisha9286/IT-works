9582204c358fb9a3ffbeab990c0c2c29c62bf59f
ava-mirtl <ava_mirtl@mail.ru>
2022-07-23 13:27:10 +0300

    json

--- js/vacancy-search.js
+++ js/vacancy-search.js
@@ -1,16 +1,39 @@
-let btn = document.querySelector('.header-search__btn');
+const { async } = require("regenerator-runtime");
+
+let btn = document.querySelector('.header-search__btn'), keyword;
 btn.addEventListener('click', (e)=>{
     e.preventDefault();
     keyWord();
+    getResultArr()
 })
-fu
+
 
  function keyWord() {
-    let input = document.querySelector('.header-search__form').value;
-    let keyword = input.toLowerCase();
+    let input = document.querySelector('.header-search__form').value,
+    list = document.querySelector('.search-container');
+    list.innerHTML="";
+    input.value = "";
+    keyword = input.toLowerCase();
     console.log(keyword);
-    const json = `./data.json`;  
-    let data = JSON.parse(json);
-    let resultArr = data.filter(el => el.title === keyword);
+return keyword}
+
+    async function fetchToDo () {
+    const responce = await fetch('https://raw.githubusercontent.com/nas-tay/WorkIT-project/main/js/data.json')
+    try {
+      data = await responce.json()
+    return data
+    } catch (error) {
+      console.log(error)
+    }}
+
+    async function getResultArr(){
+        const data = await fetchToDo()
+        let resultArr = [];
+        data.forEach((el) => {
+            if(el.title.includes(keyword)){
+        resultArr.push(el)};
+        });
+        
      console.log(resultArr);
-}
+     }
+
