569bdd78ec73d14c03081dbcfacaee7ebeddc2fe
ava-mirtl <ava_mirtl@mail.ru>
2022-07-23 19:56:19 +0300

    json

--- js/vacancy-search.js
+++ js/vacancy-search.js
@@ -9,16 +9,15 @@ btn.addEventListener('click', (e)=>{
 
 
  function keyWord() {
-    let input = document.querySelector('.header-search__form').value,
-    list = document.querySelector('.search-container');
-    list.innerHTML="";
-    input.value = "";
-    keyword = input.toLowerCase();
+    let input = document.querySelector('.header-search__form'),
+    key = input.value;
+    keyword = key.toLowerCase();
     console.log(keyword);
-return keyword}
+    input.value = "";
+    return keyword}
 
     async function fetchToDo () {
-    const responce = await fetch('https://raw.githubusercontent.com/nas-tay/WorkIT-project/main/js/data.json')
+    const responce = await fetch('https://raw.githubusercontent.com/nas-tay/WorkIT-project/dasha-new/js/data.json')
     try {
       data = await responce.json()
     return data
@@ -27,13 +26,36 @@ return keyword}
     }}
 
     async function getResultArr(){
+        document.querySelector('.search-container').innerHTML="";
         const data = await fetchToDo()
         let resultArr = [];
         data.forEach((el) => {
             if(el.title.includes(keyword)){
         resultArr.push(el)};
+       // 
         });
-        
-     console.log(resultArr);
+        console.log(resultArr);
+        resultArr.forEach(el => new Card(el.exp, el.level, el.title, el.url).newCard())
      }
 
+   
+
+            class Card{
+                constructor(exp, level, title, photo){
+                    this.exp = exp,
+                    this.level=level,
+                    this.title=title,
+                    this.photo=photo;
+                }
+            newCard(){
+                document.querySelector('.search-container').innerHTML+=`<div class='search__card'><div><h5 class='search__card-title card-title'>Должность <span class='card-title'>Уровень ${this.level}</span></h5>
+                    <div class='search__card-subtitle'>Опыт работы</div>
+                    <div class='search__card-experience'>${this.exp}</div>
+                    <div class='search__card-subtitle'>${this.title}</div>
+                    <div class='search__card-salary'>2000$</div>
+                </div>
+                <image src=${this.photo}
+                    class='search__card-photo' alt='photo' />
+            </div>`
+            }
+            }
