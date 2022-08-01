a99fd9f1264f9cccbe44c29f5fc801f722a26cbb
Anastasia Karzanova <anastasiakarzanova@Anastasias-MacBook-Pro.local>
2022-08-01 17:30:24 +0300

    added mobile filter for search_applicants

--- js/search_applicants.js
+++ js/search_applicants.js
@@ -87,7 +87,7 @@ function createObject() {
             if (!searchText == "") {
                 let search = new RegExp(searchText, "gi");
                 const rez = search.test(card.keyWords);
-                console.log(card.keyWords)
+                console.log(card.keyWords);
                 if (rez) {
                     searchResult(card);
                     newCards.push(card);
@@ -136,9 +136,6 @@ function createObject() {
         searchLevel();
     }
 
-
-
-
     // function searchSalaryFact() {
     //     list.innerHTML = "";
     //     for (card of cards) {
@@ -154,7 +151,6 @@ function createObject() {
     // if (document.querySelector('#salary').checked) {
     //     searchSalaryFact();
     // }
-
 
     function serchSalary() {
         list.innerHTML = "";
@@ -266,12 +262,10 @@ const inputSearchApp = document.querySelector("#inputSearchApp");
 const btnFilter = document.querySelector("#btnFilter");
 const btnReboot = document.querySelector("#btnReboot");
 
-
-
 inputSearchApp.onchange = () => {
     cards = firstCards;
     createObject();
-}
+};
 
 btnSearch.addEventListener("click", () => {
     cards = firstCards;
@@ -297,94 +291,103 @@ btnReboot.addEventListener("click", () => {
     cards = firstCards;
     for (card of cards) {
         searchResult(card);
-    };
+    }
 });
 
-function vacafiltermobile(screen) {
-    if (screen.matches) {
-        document.querySelector(".filter-vacancy__filter-mobile").innerHTML = `
-        <div class="filter">
-        <div class="filter-vacancy__title btnappfilter1">Фильтр</div>
-        <div class="filter-vacancy__all1">
-                        <div class="filter-vacancy__type">
-                            <div class="filter-vacancy__title">Город</div>
-                            <input type="text" name="city" id="city" placeholder="Город" list="cities">
-                            <datalist id="cities">
+// function vacafiltermobile(screen) {
+//     if (screen.matches) {
+//         document.querySelector(".filter-desktop").style.display = "none";
+//         document.querySelector(".filter-vacancy__filter-mobile").innerHTML = `
+//         <div class="filter">
+//         <div class="filter-vacancy__all1">
+//                         <div class="filter-vacancy__type">
+//                             <div class="filter-vacancy__title">Город</div>
+//                             <input type="text" name="city" id="city" placeholder="Город" list="cities">
+//                             <datalist id="cities">
 
-                            </datalist>
-                        </div>
-                        <div class=" filter__card">
-                            <div class="filter-vacancy__title">Уровень позиции</div>
-                            <div><input class="filter__card-input form-check-input level" id="junior" value="junior"
-                                    type="radio" name="level" />
-                                <label class="filter__card-label" for="junior">Junior</label></div>
-                            <div><input class="filter__card-input form-check-input level" id="middle" value="middle"
-                                    type="radio" name="level" />
-                                <label class="filter__card-label" for="middle">Middle</label></div>
-                            <div><input class="filter__card-input form-check-input level" id="senior" value="senior"
-                                    type="radio" name="level" />
-                                <label class="filter__card-label" for="senior">Senior</label></div>
-                        </div>
-                        <div class="filter__card">
-                            <div class="filter-vacancy__title">Опыт работы</div>
-                            <div><input class="filter__card-input form-check-input experience" type="radio"
-                                    name="experience" value="zero" id="zero" />
-                                <label class="filter__card-label " for="zero"> Менее года</label></div>
-                            <div><input class="filter__card-input form-check-input experience" type="radio"
-                                    name="experience" value="medium" id="medium" />
-                                <label class="filter__card-label " for="medium"> От 3 до 6 лет</label></div>
-                            <div><input class="filter__card-input form-check-input experience" type="radio"
-                                    name="experience" value="large" id="large" />
-                                <label class="filter__card-label " name="large"> Более 6 лет</label></div>
-                        </div>
-                        <div class="filter__card">
-                            <div class="filter-vacancy__title">Уровень дохода $</div>
-                            <div class="filter__card-salary">
-                                <input class="filter-vacancy__input-salary" type="number" placeholder="From"
-                                    id="minSalary" />
-                                <input class="filter-vacancy__input-salary" type="number" placeholder="To"
-                                    id="maxSalary" /></div>
-                            <div>
-                                <input class="filter__card-input form-check-input" type="checkbox" name="salary" />
-                                <label class="filter__card-label" for="salary">Указан доход</label></div>
-                        </div>
-                        <div class="filter__card">
-                            <div class="filter-vacancy__title">Формат работы</div>
-                            <div><input class="filter__card-input form-check-input format" id="distant"
-                                    value="удаленный" type="checkbox" />
-                                <label class="filter__card-label">Удаленный</label></div>
-                            <div><input class="filter__card-input form-check-input format" id="office" value="офис"
-                                    type="checkbox" />
-                                <label class="filter__card-label">Офис</label></div>
-                            <div><input class="filter__card-input form-check-input format" id="hybrid" value="гибрид"
-                                    type="checkbox" />
-                                <label class="filter__card-label">Гибрид</label></div>
-                        </div>
-                        <div class="filter-vacancy__btns">
-                            <div class="filter-vacancy__change" id="btnFilter">Применить</div>
-                            <div class="filter-vacancy__throwoff" id="btnReboot">Сбросить</div>
-                        </div>
-                    </div>
-                    </div>
-        `;
-    }
-}
+//                             </datalist>
+//                         </div>
+//                         <div class=" filter__card">
+//                             <div class="filter-vacancy__title">Уровень позиции</div>
+//                             <div><input class="filter__card-input form-check-input level" id="junior" value="junior"
+//                                     type="radio" name="level" />
+//                                 <label class="filter__card-label" for="junior">Junior</label></div>
+//                             <div><input class="filter__card-input form-check-input level" id="middle" value="middle"
+//                                     type="radio" name="level" />
+//                                 <label class="filter__card-label" for="middle">Middle</label></div>
+//                             <div><input class="filter__card-input form-check-input level" id="senior" value="senior"
+//                                     type="radio" name="level" />
+//                                 <label class="filter__card-label" for="senior">Senior</label></div>
+//                         </div>
+//                         <div class="filter__card">
+//                         <div class="filter-vacancy__title">Опыт работы</div>
+//                         <div><input class="filter__card-input form-check-input experience" type="radio"
+//                                 name="experience" value="zero" id="zero" />
+//                             <label class="filter__card-label " for="zero"> Менее года</label></div>
+//                             <div><input class="filter__card-input form-check-input experience" type="radio"
+//                                 name="experience" value="small" id="small" />
+//                             <label class=" filter__card-label " for=" small"> От 1 до 3 лет</label></div>
+//                         <div><input class="filter__card-input form-check-input experience" type="radio"
+//                                 name="experience" value="small" id="small" />
+//                             <label class="filter__card-label " for="small"> От 1 года до 3 лет</label></div>
+//                         <div><input class="filter__card-input form-check-input experience" type="radio"
+//                                 name="experience" value="medium" id="medium" />
+//                             <label class="filter__card-label " for="medium"> От 3 до 6 лет</label></div>
+//                         <div><input class="filter__card-input form-check-input experience" type="radio"
+//                                 name="experience" value="large" id="large" />
+//                             <label class="filter__card-label " name="large"> Более 6 лет</label></div>
+//                         </div>
+//                         <div class="filter__card">
+//                             <div class="filter-vacancy__title">Уровень дохода $</div>
+//                             <div class="filter__card-salary">
+//                                 <input class="filter-vacancy__input-salary" type="number" placeholder="From"
+//                                     id="minSalary" />
+//                                 <input class="filter-vacancy__input-salary" type="number" placeholder="To"
+//                                     id="maxSalary" /></div>
+//                             <div>
+//                                 <input class="filter__card-input form-check-input" type="checkbox" name="salary" />
+//                                 <label class="filter__card-label" for="salary">Указан доход</label></div>
+//                         </div>
+//                         <div class="filter__card">
+//                             <div class="filter-vacancy__title">Формат работы</div>
+//                             <div><input class="filter__card-input form-check-input format" id="distant"
+//                                     value="удаленный" type="checkbox" />
+//                                 <label class="filter__card-label">Удаленный</label></div>
+//                             <div><input class="filter__card-input form-check-input format" id="office" value="офис"
+//                                     type="checkbox" />
+//                                 <label class="filter__card-label">Офис</label></div>
+//                             <div><input class="filter__card-input form-check-input format" id="hybrid" value="гибрид"
+//                                     type="checkbox" />
+//                                 <label class="filter__card-label">Гибрид</label></div>
+//                         </div>
+//                         <div class="filter-vacancy__btns">
+//                             <div class="filter-vacancy__change" id="btnFilter">Применить</div>
+//                             <div class="filter-vacancy__throwoff" id="btnReboot">Сбросить</div>
+//                         </div>
+//                     </div>
+//                     </div>
+//         `;
+//     }
+// }
 
-let screen = window.matchMedia("(max-width: 797px)");
-vacafiltermobile(screen);
-screen.addEventListener("DOMContentLoaded", vacafiltermobile);
+// let screen = window.matchMedia("(max-width: 797px)");
+// vacafiltermobile(screen);
+// screen.addEventListener("DOMContentLoaded", vacafiltermobile);
 
 function filtermobile() {
-    var ele = document.querySelector(".filter-vacancy__all1");
-    var text = document.querySelector(".btnappfilter1");
+    var ele = document.querySelector(".filter-content");
+    var text = document.querySelector(".filter-mobile");
     if (ele.style.display == "block") {
         ele.style.display = "none";
         text.innerHTML = "Фильтр";
+        document.querySelector(".searching__filters").classList.remove("shadow-on");
     } else {
         ele.style.display = "block";
         text.innerHTML = "Скрыть фильтр";
+        document.querySelector(".searching__filters").classList.remove("searching__filters_shadow");
+        document.querySelector(".searching__filters").classList.add("shadow-on");
     }
 }
-document.getElementById('btnappfilter').addEventListener('click', () => {
+document.getElementById("btnappfilter1").addEventListener("click", () => {
     filtermobile();
 });
